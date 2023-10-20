bcac5e78b070dc2097fb034fffd48579ba0691b7
Sara Cano <127054269+saritacanosierra@users.noreply.github.com>
2023-10-19 22:05:31 -0500

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// // <!-- 1. Una agencia de viajes desea llevar un control de todos los servicios de turismo que ofrece. 
+// // La agencia presta servicios de reserva de paquetes turísticos, y ofrece 3 diferentes, de los 
+// // cuales se tiene el comportamiento: 
+// // a. Para el paquete 1 se hace una reserva a la ciudad de México, la cantidad de 
+// // personas debe ser mínimo 3 y máximo 5, el valor por persona es de 5.000.000
+// // b. Para el paquete 2 se hace una reserva a la ciudad de Brasil, solo pueden viajar 2 
+// // personas y ninguna de ellas debe ser menor de edad. El valor del viaje por persona 
+// // es de 4.500.000
+// // c. Para el paquete 3 se hace una reserva a la ciudad de Ámsterdam, máximo para 2 
+// // persona. El valor por persona es de 7.500.000, y si alguna persona es menor de 
+// // edad se le hace un descuento del 25%
+// // Finalizando el día, es necesario mostrar las ganancias por cada paquete, mostrar cuentas 
+// // personas viajarán a México, cuantas viajarán a Ámsterdam y cuantas a Brasil. Mostrar 
+// // cuantas personas son menores de edad
+// // Recordar que es necesario almacenar la información de las personas que viajan (nombre, 
+// // documento, teléfono y edad) ya que debe existir una función que muestre la información 
+// // de las personas que se han registrado -->
+
+
+
+// // <!-- definir variables -->
+
+
+// function register(){
+//     let ciudad= document.getElementById('ciudad')
+
+
+//  switch (paquetes) {
+//     case a:
+//         if( ciudad== mexico && personas>=3 && personas<=5){
+//             let total=personas*5000000;
+//         }
+// console.log('el valor a pagar por este paque tes de: '+total)
+        
+//         break;
+ 
+//     default:
+//         break;
+//  }
+
+while (repetir) {
+    opcion = Number (prompt ("Seleccione: \n1 - Registar\n2 - Salir"));
+    persona = {
+        nombre: prompt ("Ingrese su nobre"), edad: prompt ("Ingrese su edad:"),
+    };
+    personas.push (persona);
+    if (opcion == 2) {
+    repetir=false;
+    }
+}
+
+// } fin funtion onclick
+    
+ 
+// <!-- leer datos -->
+// <!-- operaciones -->
+// <!-- imprimir --></body> */}
